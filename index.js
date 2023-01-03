02dfbaa15e7a0fc604877a680a848efacada8ce7
surajgit036 <Surajkrch36@gmail.com>
2023-01-03 11:23:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+window.onload = function () {
+    var seconds = 00;
+    var tens = 00;
+    var Interval;
+  
+  
+    //All the html elements that we will use
+    var appendTens = document.getElementById("tens");
+    var appendSeconds = document.getElementById("seconds");
+    var buttonStart = document.getElementById("button-start");
+    var buttonStop = document.getElementById("button-stop");
+    var buttonReset = document.getElementById("button-reset");
+  
+    //Implementing the start button
+    buttonStart.onclick = function () {
+      clearInterval(Interval);
+      Interval = setInterval(startTimer, 10);
+    };
+  
+    //Implementing the stop button
+    buttonStop.onclick = function () {
+      clearInterval(Interval);
+    };
+  
+    //Implementing the reset button
+    buttonReset.onclick = function () {
+      clearInterval(Interval);
+      tens = "00";
+      seconds = "00";
+      appendTens.innerHTML = tens;
+      appendSeconds.innerHTML = seconds;
+    };
+  
+  
+    //Our main function
+    function startTimer() {
+      tens++;
+  
+      if (tens <= 9) {
+        appendTens.innerHTML = "0" + tens;
+      }
+  
+      if (tens > 9) {
+        appendTens.innerHTML = tens;
+      }
+  
+      if (tens > 99) {
+        console.log("seconds");
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+      }
+  
+      if (seconds > 9) {
+        appendSeconds.innerHTML = seconds;
+      }
+    }
+  };
